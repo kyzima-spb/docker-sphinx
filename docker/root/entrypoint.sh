6374e4c23ebdc7554a4871f739907b123484ca15
Kirill Vercetti <office@kyzima-spb.com>
2023-11-09 19:15:41 +0300

    Auto create new project from environment variables

--- docker/root/entrypoint.sh
+++ docker/root/entrypoint.sh
@@ -1,53 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-PACKAGE_DIR='/package'
-LOG_FILE='/var/log/package_install.log'
+packageDir="$(pwd)"
+SPHINX_PATH=${SPHINX_PATH:-"$packageDir/docs"}
 
+[[ ! -d "$SPHINX_PATH" ]] && mkdir -p "$SPHINX_PATH"
 
-install() {
-    pushd "$PACKAGE_DIR"
-        if [[ -f requirements.txt ]]; then
-            pip install --disable-pip-version-check -r requirements.txt
-        elif [[ -f setup.py ]]; then
-            pip install --disable-pip-version-check -e /package | tee "$LOG_FILE"
+find "$SPHINX_PATH" -maxdepth 0 -empty | xargs -r -n1 create-project
 
-            for i in $EXTRA; do
-                echo "Install extra requires $i"
-                pip install --disable-pip-version-check -e "/package[$i]" | tee -a "$LOG_FILE"
-            done
-        else
-            echo "Package not installed."
-        fi
-    popd
-}
+if [[ -z "$(pip freeze | grep -F "-e $packageDir")" ]]
+then
+    if [[ -f requirements.txt ]]
+    then
+        pip install -r requirements.txt
+    elif [[ -f setup.py ]] || [[ -f pyproject.toml ]]
+    then
+        pip install -e .
 
-
-if [[ ! -f "$LOG_FILE" ]]; then
-    echo "Install package"
-    install
-fi
-
-
-if [[ -z $USER_UID ]]; then
-    USER_UID=$(id -u)
-fi
-
-if [[ -z $USER_GID ]]; then
-    USER_GID=$(id -g)
-fi
-
-
-if [[ "$1" =~ ^sphinx ]]; then
-    if [[ "$(id -u)" = '0' ]]; then
-        chown -R $USER_UID:$USER_GID /home/user "$(pwd)" /build
-        usermod -u $USER_UID user
-        groupmod -g $USER_GID user
-        exec gosu user "$BASH_SOURCE" "$@"
-    else
-        echo "Run Sphinx CLI; User: $(id -u), Command: $@"
+        for i in $EXTRA; do
+            pip install --user -e ".[$i]"
+        done
     fi
 fi
-
 
 exec "$@"
